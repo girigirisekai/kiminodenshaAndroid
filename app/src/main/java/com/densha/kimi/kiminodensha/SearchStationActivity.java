068aac09088ecb65586117413214243b813c9157
jaeyong <jaeyong4536@gmail.com>
2017-07-25 10:27:58 +0900

    test

--- app/src/main/java/com/densha/kimi/kiminodensha/SearchStationActivity.java
+++ app/src/main/java/com/densha/kimi/kiminodensha/SearchStationActivity.java
@@ -1,5 +1,6 @@
 package com.densha.kimi.kiminodensha;
 
+import android.app.LauncherActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,6 +38,10 @@ public class SearchStationActivity extends AppCompatActivity {
     ListView lists;
     ArrayList <String> asd  = new ArrayList<>();
     ArrayList<String> lineNameArray = new ArrayList<>();
+    ArrayAdapter adapter;
+    String selectedItem;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +49,6 @@ public class SearchStationActivity extends AppCompatActivity {
         // searchStationName_favorite xml 사용
         setContentView(R.layout.activity_searchstation);
 
-        //나중에 id intent연결
         Intent intent=getIntent();
         id=intent.getStringExtra("id");
         Log.d("아이디",id);
@@ -56,12 +61,9 @@ public class SearchStationActivity extends AppCompatActivity {
         Button btn = (Button)findViewById(R.id.searchBtn);
         edtxt = (EditText) findViewById(R.id.searchEditBox);
         txt = (TextView) findViewById(R.id.SearchResult);
-
-
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, lineNameArray);
-
         lists = (ListView) findViewById(R.id.stationList);
-        lists.setAdapter(adapter);
+
+
 
         // Station 정보 버튼
         btn.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +78,16 @@ public class SearchStationActivity extends AppCompatActivity {
 
             }
         });
+
+        lists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedItem = (String) adapter.getItem(i); // 아이템 나오는 부분
+                Toast.makeText(SearchStationActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
+                saveSelectedItem();
+            }
+        });
+
 
     }
 
@@ -164,11 +176,13 @@ public class SearchStationActivity extends AppCompatActivity {
                                 JSONObject bufferobj = new JSONObject(resultArray.get(i).toString());
                                 String linei = bufferobj.getString("line");
                                 String namei = bufferobj.getString("name");
-                                asd.add(linei +", "+namei);
+                                asd.add(linei +","+namei);
                                 texts+= linei +", "+namei;
                             }
                             txt.setText(asd.toString());
-
+                            lineNameArray = asd;
+                            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, lineNameArray);
+                            lists.setAdapter(adapter);
 
 
                             break;
@@ -180,5 +194,87 @@ public class SearchStationActivity extends AppCompatActivity {
 
             Toast.makeText(this, ""+e.toString(), Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    public void saveSelectedItem(){
+
+        URL url = null;
+        HttpURLConnection con = null;
+        StringBuilder sb = new StringBuilder();
+
+        //JSON형태로 보내기 준비
+        String [] getSelectedItem = selectedItem.split(",");
+        Log.d("LINE",getSelectedItem[0]);
+        Log.d("STATIONNAME",getSelectedItem[1]);
+        // 아이디 라인 이름 넘기기
+        JSONObject makeJson = new JSONObject();
+        try{
+            makeJson.put("ID",id);
+            makeJson.put("LINE",getSelectedItem[0]);
+            makeJson.put("STATIONNAME",getSelectedItem[1]);
+
+        }catch(Exception c){
+
+        }
+
+        try{
+
+            Log.d("ip주소 ","203.233.196.139");
+            //http://10.0.2.2:8888
+            url = new URL("http://10.0.2.2:8888/densha/androidNewFavorite");
+            // 나중에 ip 주소 바꿔주세요
+
+        }catch(Exception e){
+            Toast.makeText(this, "잘못된 주소입니다!", Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d("id", id);
+        try{
+            con = (HttpURLConnection)url.openConnection();
+
+            if(con != null){
+                con.setConnectTimeout(10000);
+                con.setUseCaches(false);
+                con.setRequestMethod("POST");
+                //JSON형태로 보내기 설정
+                con.setRequestProperty("Content-type", "application/json");
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                OutputStream out = con.getOutputStream();
+                out.write(makeJson.toString().getBytes("UTF-8"));
+                out.flush();
+                out.close();
+
+                if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    InputStreamReader in = new InputStreamReader(con.getInputStream());
+                    int ch;
+                    while ((ch = in.read()) != -1){
+                        sb.append((char)ch);
+                    }
+                    in.close();
+                    String jsonResult=sb.toString();
+                    Log.d("결과", jsonResult);
+                    JSONObject resultJson = new JSONObject(jsonResult);
+                    String result = resultJson.getString("RESULT"); //RESULT 분석
+                    //서버로부터 수신된 데이터 학인
+                    //1. 전송실패
+                    switch (result) {
+                        case "False": // 실패
+                            Log.d("Favorite", "False");
+                            txt.setText("실패" + result);
+                            break;
+                        case "True": //성공
+                            Toast.makeText(this, "등록되었습니다.", Toast.LENGTH_SHORT).show();
+                            txt.setText("성공" + result);
+                            break;
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(this, ""+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
