68f2785f567034431bcc6e8f97daa8afde68a2ad
jaeyong <jaeyong4536@gmail.com>
2017-07-28 17:52:56 +0900

    test

--- app/src/main/java/com/densha/kimi/kiminodensha/CarActivity.java
+++ app/src/main/java/com/densha/kimi/kiminodensha/CarActivity.java
@@ -2,15 +2,19 @@ package com.densha.kimi.kiminodensha;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,10 +30,14 @@ import java.util.ArrayList;
 public class CarActivity extends AppCompatActivity {
 
     //변수
-    String subwayNum="2002";
+    Intent intent;
+    String subwayNum="";
     JSONObject json=null;
     String subwayNumInfo="";
-    String[] subwayInfo=new String[10];
+    JSONArray jsonArray=null;
+    ArrayList<String> subwayInfoArray=null;
+    JSONObject subwayInfo=null;
+    TextView[] carCongestionArray=new TextView[10];
 
     //메소드
     //onCreate
@@ -38,17 +46,69 @@ public class CarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car);
 
-        Intent intent=getIntent();
-        intent.getStringExtra("num");
+        // 화면을 가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+        //차량번호 로드
+        intent=getIntent();
+        subwayNum=Integer.toString(intent.getIntExtra("num",0));
+        Log.d("차량번호",subwayNum);
 
         //서버 전송을 위한 설정
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        //열개의 량의 데이터를 가져옴
-        //데이터는 사람수, 혼잡도율
-        //혼잡율에 따라 칸의 색깔 다름  ---> 빨강
-        Log.d("칸인원수로드", "실행");
+        carCongestionArray[0]=(TextView)findViewById(R.id.tText1Con);
+        carCongestionArray[1]=(TextView)findViewById(R.id.tText2Con);
+        carCongestionArray[2]=(TextView)findViewById(R.id.tText3Con);
+        carCongestionArray[3]=(TextView)findViewById(R.id.tText4Con);
+        carCongestionArray[4]=(TextView)findViewById(R.id.tText5Con);
+        carCongestionArray[5]=(TextView)findViewById(R.id.tText6Con);
+        carCongestionArray[6]=(TextView)findViewById(R.id.tText7Con);
+        carCongestionArray[7]=(TextView)findViewById(R.id.tText8Con);
+        carCongestionArray[8]=(TextView)findViewById(R.id.tText9Con);
+        carCongestionArray[9]=(TextView)findViewById(R.id.tText10Con);
+
+        carCongestionLoad();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_car);
+
+        // 화면을 가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //차량번호 로드
+        intent=getIntent();
+        subwayNum=Integer.toString(intent.getIntExtra("num",0));
+        Log.d("차량번호",subwayNum);
+
+        //서버 전송을 위한 설정
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        carCongestionArray[0]=(TextView)findViewById(R.id.tText1Con);
+        carCongestionArray[1]=(TextView)findViewById(R.id.tText2Con);
+        carCongestionArray[2]=(TextView)findViewById(R.id.tText3Con);
+        carCongestionArray[3]=(TextView)findViewById(R.id.tText4Con);
+        carCongestionArray[4]=(TextView)findViewById(R.id.tText5Con);
+        carCongestionArray[5]=(TextView)findViewById(R.id.tText6Con);
+        carCongestionArray[6]=(TextView)findViewById(R.id.tText7Con);
+        carCongestionArray[7]=(TextView)findViewById(R.id.tText8Con);
+        carCongestionArray[8]=(TextView)findViewById(R.id.tText9Con);
+        carCongestionArray[9]=(TextView)findViewById(R.id.tText10Con);
+
+        carCongestionLoad();
+    }
+
+    //열개의 량의 데이터를 가져옴
+    //데이터는 사람수, 혼잡도율
+    //혼잡율에 따라 칸의 색깔 다름  ---> 빨강
+    public void carCongestionLoad(){
+        Log.d("량 데이터 로드", "실행");
 
         URL url = null;
         HttpURLConnection con = null;
@@ -66,7 +126,7 @@ public class CarActivity extends AppCompatActivity {
         //url설정
         try {
             Log.d("ip주소 ", "203.233.196.139");
-            url = new URL("http://203.233.196.139:8888/densha/apiservice/json?subwaynum=2002");
+            url = new URL("http://203.233.196.139:8888/densha/apiservice/json?subwaynum="+subwayNum);
         } catch (MalformedURLException e) {
             Toast.makeText(this, "잘못된 주소입니다!", Toast.LENGTH_SHORT).show();
         }
@@ -93,10 +153,15 @@ public class CarActivity extends AppCompatActivity {
                     //수신된 데이터 학인
                     JSONObject jsonObject = new JSONObject(sb.toString());
                     String result = jsonObject.getString("item");
-                    Log.d("수신데이터",result);
-                   /* result=result.replace("[","");
-                    result=result.replace("]","");
-                    Log.d("수신데이터2",result);*/
+                    Log.d("수신데이터들",result);
+                    jsonArray=new JSONArray(result);
+                    subwayInfo=new JSONObject();
+
+                    for(int i=0;i<jsonArray.length();i++){
+                        Log.d("수신데이터"+(i+1),jsonArray.get(i).toString());
+                        subwayInfo=(JSONObject) jsonArray.get(i);
+                        carCongestionArray[i].setText(subwayInfo.getString("humanNum")+"명");
+                    }
                 }
             }
         } catch (Exception e) {
