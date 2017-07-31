71684362dcb2ebe0a376778c2585436ddd7a2646
jaeyong <jaeyong4536@gmail.com>
2017-07-31 09:54:09 +0900

    test

--- app/src/main/java/com/densha/kimi/kiminodensha/CarActivity.java
+++ app/src/main/java/com/densha/kimi/kiminodensha/CarActivity.java
@@ -3,11 +3,15 @@ package com.densha.kimi.kiminodensha;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,7 +41,11 @@ public class CarActivity extends AppCompatActivity {
     JSONArray jsonArray=null;
     ArrayList<String> subwayInfoArray=null;
     JSONObject subwayInfo=null;
-    TextView[] carCongestionArray=new TextView[10];
+    TextView[] carHumanNumArray=new TextView[10];
+    String[] carCongestionArray=new String[10];
+    ImageView[] carImageArray=new ImageView[10];
+    String elderlySeats="";
+    String[] elderlySeatsArray=new String[10];
 
     //메소드
     //onCreate
@@ -49,7 +57,6 @@ public class CarActivity extends AppCompatActivity {
         // 화면을 가로 고정
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-
         //차량번호 로드
         intent=getIntent();
         subwayNum=Integer.toString(intent.getIntExtra("num",0));
@@ -59,20 +66,36 @@ public class CarActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        carCongestionArray[0]=(TextView)findViewById(R.id.tText1Con);
-        carCongestionArray[1]=(TextView)findViewById(R.id.tText2Con);
-        carCongestionArray[2]=(TextView)findViewById(R.id.tText3Con);
-        carCongestionArray[3]=(TextView)findViewById(R.id.tText4Con);
-        carCongestionArray[4]=(TextView)findViewById(R.id.tText5Con);
-        carCongestionArray[5]=(TextView)findViewById(R.id.tText6Con);
-        carCongestionArray[6]=(TextView)findViewById(R.id.tText7Con);
-        carCongestionArray[7]=(TextView)findViewById(R.id.tText8Con);
-        carCongestionArray[8]=(TextView)findViewById(R.id.tText9Con);
-        carCongestionArray[9]=(TextView)findViewById(R.id.tText10Con);
+        //차량 혼잡도 Textview 로드
+        carHumanNumArray[0]=(TextView)findViewById(R.id.tText1Con);
+        carHumanNumArray[1]=(TextView)findViewById(R.id.tText2Con);
+        carHumanNumArray[2]=(TextView)findViewById(R.id.tText3Con);
+        carHumanNumArray[3]=(TextView)findViewById(R.id.tText4Con);
+        carHumanNumArray[4]=(TextView)findViewById(R.id.tText5Con);
+        carHumanNumArray[5]=(TextView)findViewById(R.id.tText6Con);
+        carHumanNumArray[6]=(TextView)findViewById(R.id.tText7Con);
+        carHumanNumArray[7]=(TextView)findViewById(R.id.tText8Con);
+        carHumanNumArray[8]=(TextView)findViewById(R.id.tText9Con);
+        carHumanNumArray[9]=(TextView)findViewById(R.id.tText10Con);
 
+        //차량 Imageview 로드
+        carImageArray[0]=(ImageView)findViewById(R.id.car1);
+        carImageArray[1]=(ImageView)findViewById(R.id.car2);
+        carImageArray[2]=(ImageView)findViewById(R.id.car3);
+        carImageArray[3]=(ImageView)findViewById(R.id.car4);
+        carImageArray[4]=(ImageView)findViewById(R.id.car5);
+        carImageArray[5]=(ImageView)findViewById(R.id.car6);
+        carImageArray[6]=(ImageView)findViewById(R.id.car7);
+        carImageArray[7]=(ImageView)findViewById(R.id.car8);
+        carImageArray[8]=(ImageView)findViewById(R.id.car9);
+        carImageArray[9]=(ImageView)findViewById(R.id.car10);
+
+        //차량 혼잡도 로드
         carCongestionLoad();
+
     }
 
+    //
     @Override
     protected void onResume() {
         super.onResume();
@@ -90,23 +113,36 @@ public class CarActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        carCongestionArray[0]=(TextView)findViewById(R.id.tText1Con);
-        carCongestionArray[1]=(TextView)findViewById(R.id.tText2Con);
-        carCongestionArray[2]=(TextView)findViewById(R.id.tText3Con);
-        carCongestionArray[3]=(TextView)findViewById(R.id.tText4Con);
-        carCongestionArray[4]=(TextView)findViewById(R.id.tText5Con);
-        carCongestionArray[5]=(TextView)findViewById(R.id.tText6Con);
-        carCongestionArray[6]=(TextView)findViewById(R.id.tText7Con);
-        carCongestionArray[7]=(TextView)findViewById(R.id.tText8Con);
-        carCongestionArray[8]=(TextView)findViewById(R.id.tText9Con);
-        carCongestionArray[9]=(TextView)findViewById(R.id.tText10Con);
+        //차량 혼잡도 Textview 로드
+        carHumanNumArray[0]=(TextView)findViewById(R.id.tText1Con);
+        carHumanNumArray[1]=(TextView)findViewById(R.id.tText2Con);
+        carHumanNumArray[2]=(TextView)findViewById(R.id.tText3Con);
+        carHumanNumArray[3]=(TextView)findViewById(R.id.tText4Con);
+        carHumanNumArray[4]=(TextView)findViewById(R.id.tText5Con);
+        carHumanNumArray[5]=(TextView)findViewById(R.id.tText6Con);
+        carHumanNumArray[6]=(TextView)findViewById(R.id.tText7Con);
+        carHumanNumArray[7]=(TextView)findViewById(R.id.tText8Con);
+        carHumanNumArray[8]=(TextView)findViewById(R.id.tText9Con);
+        carHumanNumArray[9]=(TextView)findViewById(R.id.tText10Con);
 
+        //차량 Imageview 로드
+        carImageArray[0]=(ImageView)findViewById(R.id.car1);
+        carImageArray[1]=(ImageView)findViewById(R.id.car2);
+        carImageArray[2]=(ImageView)findViewById(R.id.car3);
+        carImageArray[3]=(ImageView)findViewById(R.id.car4);
+        carImageArray[4]=(ImageView)findViewById(R.id.car5);
+        carImageArray[5]=(ImageView)findViewById(R.id.car6);
+        carImageArray[6]=(ImageView)findViewById(R.id.car7);
+        carImageArray[7]=(ImageView)findViewById(R.id.car8);
+        carImageArray[8]=(ImageView)findViewById(R.id.car9);
+        carImageArray[9]=(ImageView)findViewById(R.id.car10);
+
+        //차량 혼잡도 로드
         carCongestionLoad();
     }
 
     //열개의 량의 데이터를 가져옴
-    //데이터는 사람수, 혼잡도율
-    //혼잡율에 따라 칸의 색깔 다름  ---> 빨강
+    //데이터는 사람수, 혼잡도율 사용
     public void carCongestionLoad(){
         Log.d("량 데이터 로드", "실행");
 
@@ -150,6 +186,7 @@ public class CarActivity extends AppCompatActivity {
                     }
                     in.close();
                     Log.d("수신", "완료");
+
                     //수신된 데이터 학인
                     JSONObject jsonObject = new JSONObject(sb.toString());
                     String result = jsonObject.getString("item");
@@ -157,11 +194,64 @@ public class CarActivity extends AppCompatActivity {
                     jsonArray=new JSONArray(result);
                     subwayInfo=new JSONObject();
 
-                    for(int i=0;i<jsonArray.length();i++){
-                        Log.d("수신데이터"+(i+1),jsonArray.get(i).toString());
-                        subwayInfo=(JSONObject) jsonArray.get(i);
-                        carCongestionArray[i].setText(subwayInfo.getString("humanNum")+"명");
+                    //인원수와 혼잡도, 노약자석
+                    for(int i=0;i<jsonArray.length();i++) {
+                        Log.d("수신데이터" + (i + 1), jsonArray.get(i).toString());
+                        subwayInfo = (JSONObject) jsonArray.get(i);
+                        carHumanNumArray[i].setText(subwayInfo.getString("humanNum") + "명");
+                        carCongestionArray[i]=subwayInfo.getString("humanPercent");
+                        for (int j = 1; j <= 12; j++) {
+                            elderlySeats += subwayInfo.getString("elderlySeat" +j);
+                        }
+                        elderlySeatsArray[i]=elderlySeats;
+                        elderlySeats="";
                     }
+
+                    //칸 색상
+                    //0~10% = 그대로, 11~30% = 연분홍, 31~70% = 진한 분홍, 71~100% = 빨강
+                    Log.d("칸 색상 변화","실행");
+                    for(int i=0;i<jsonArray.length();i++){
+                        int congestion=Integer.parseInt(carCongestionArray[i]);
+                        if(congestion>=0&&congestion<=10) {
+                            Log.d("칸 색상","변화없음");
+                        }else if(congestion>=11&&congestion<=30){
+                            carImageArray[i].setColorFilter(0XFFDBC1);
+                            Log.d("칸 색상","연분홍");
+                        }else if(congestion>=31&&congestion<=70){
+                            carImageArray[i].setColorFilter(0XFF88A7);
+                            Log.d("칸 색상","진한 분홍");
+                        }else if(congestion>=71&&congestion<=100){
+                            carImageArray[i].setColorFilter(0XFF5675);
+                            Log.d("칸 색상","빨간색");
+                        }
+                    }
+                    Log.d("칸 색상 변화","완료");
+                    Log.d("칸 클릭 설정","실행");
+                    //칸 클릭 설정
+
+                    for(int i=0;i<carImageArray.length;i++) {
+                        final int j=i;
+                        carImageArray[j].setOnTouchListener(new View.OnTouchListener() {
+                            @Override
+                            public boolean onTouch(View view, MotionEvent motionEvent) {
+                                Log.d("노약자석", "실행");
+                                //버튼 눌릴 시
+                                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                                    String elderlySeatstoElderly=elderlySeatsArray[j];
+                                    String carNum=Integer.toString(j);
+                                    intent = new Intent(CarActivity.this, ElderlySeatsActivity.class);
+                                    Log.d("노약자석 데이터",elderlySeatstoElderly);
+                                    Log.d("차량번호",carNum);
+                                    intent.putExtra("elderlySeats",elderlySeatstoElderly);
+                                    intent.putExtra("carNum",carNum);
+                                    startActivity(intent);
+                                    Log.d("노약자석","완료");
+                                }
+                                return true;
+                            }
+                        });
+                    }
+                    Log.d("칸 클릭 설정","완료");
                 }
             }
         } catch (Exception e) {
--- app/src/main/java/com/densha/kimi/kiminodensha/ElderlySeatsActivity.java
+++ app/src/main/java/com/densha/kimi/kiminodensha/ElderlySeatsActivity.java
@@ -1,19 +1,105 @@
 package com.densha.kimi.kiminodensha;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
 /**
  * ElderlySeats
  */
 public class ElderlySeatsActivity extends AppCompatActivity {
 
+    //변수
+    Intent intent=null;
+    String carNum="";
+    String elderlySeats="";
+    static int elderlySeatsLength=12;
+    ImageView[] imageViewArray=new ImageView[12];
     //메소드
     //onCreate
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat);
+
+        // 화면을 가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //칸번호와 노약자석 정보 로드
+        intent=getIntent();
+        carNum=intent.getStringExtra("carNum");
+        elderlySeats=intent.getStringExtra("elderlySeats");
+        Log.d("칸번호",carNum);
+        Log.d("노약자석",elderlySeats);
+
+        //이미지뷰 로드
+        imageViewArray[0]=(ImageView)findViewById(R.id.pokemon1);
+        imageViewArray[1]=(ImageView)findViewById(R.id.pokemon2);
+        imageViewArray[2]=(ImageView)findViewById(R.id.pokemon3);
+        imageViewArray[3]=(ImageView)findViewById(R.id.pokemon4);
+        imageViewArray[4]=(ImageView)findViewById(R.id.pokemon5);
+        imageViewArray[5]=(ImageView)findViewById(R.id.pokemon6);
+        imageViewArray[6]=(ImageView)findViewById(R.id.pokemon7);
+        imageViewArray[7]=(ImageView)findViewById(R.id.pokemon8);
+        imageViewArray[8]=(ImageView)findViewById(R.id.pokemon9);
+        imageViewArray[9]=(ImageView)findViewById(R.id.pokemon10);
+        imageViewArray[10]=(ImageView)findViewById(R.id.pokemon11);
+        imageViewArray[11]=(ImageView)findViewById(R.id.pokemon12);
+
+        //이미지 색 초기화
+        for(int i=0;i<imageViewArray.length;i++){
+            imageViewArray[i].setColorFilter(Color.BLACK);
+        }
+
+        //사람 유무로 이미지 색 변경
+        for(int i=0;i<elderlySeatsLength;i++){
+            if(elderlySeats.charAt(i)=='1'){
+                imageViewArray[i].setColorFilter(null);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_seat);
+
+        //칸번호와 노약자석 정보 로드
+        intent=getIntent();
+        carNum=intent.getStringExtra("carNum");
+        elderlySeats=intent.getStringExtra("elderlySeats");
+        Log.d("칸번호",carNum);
+        Log.d("노약자석",elderlySeats);
+
+        //이미지뷰 로드
+        imageViewArray[0]=(ImageView)findViewById(R.id.pokemon1);
+        imageViewArray[1]=(ImageView)findViewById(R.id.pokemon2);
+        imageViewArray[2]=(ImageView)findViewById(R.id.pokemon3);
+        imageViewArray[3]=(ImageView)findViewById(R.id.pokemon4);
+        imageViewArray[4]=(ImageView)findViewById(R.id.pokemon5);
+        imageViewArray[5]=(ImageView)findViewById(R.id.pokemon6);
+        imageViewArray[6]=(ImageView)findViewById(R.id.pokemon7);
+        imageViewArray[7]=(ImageView)findViewById(R.id.pokemon8);
+        imageViewArray[8]=(ImageView)findViewById(R.id.pokemon9);
+        imageViewArray[9]=(ImageView)findViewById(R.id.pokemon10);
+        imageViewArray[10]=(ImageView)findViewById(R.id.pokemon11);
+        imageViewArray[11]=(ImageView)findViewById(R.id.pokemon12);
+
+        //이미지 색 초기화
+        for(int i=0;i<imageViewArray.length;i++){
+            imageViewArray[i].setColorFilter(Color.GRAY);
+        }
+
+        //사람 유무로 이미지 색 변경
+        for(int i=0;i<elderlySeatsLength;i++){
+            if(elderlySeats.charAt(i)=='1'){
+                imageViewArray[i].setColorFilter(null);
+            }
+        }
     }
 }
