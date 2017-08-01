58238ac02294e6d9ba40ccd6a8786a3b273959c6
jaeyong <jaeyong4536@gmail.com>
2017-08-01 12:05:49 +0900

    test

--- app/src/main/java/com/densha/kimi/kiminodensha/ElderlySeatsActivity.java
+++ app/src/main/java/com/densha/kimi/kiminodensha/ElderlySeatsActivity.java
@@ -4,12 +4,24 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * ElderlySeats
@@ -18,10 +30,16 @@ public class ElderlySeatsActivity extends AppCompatActivity {
 
     //변수
     Intent intent=null;
+    JSONObject json=null;
+    String subwayNum="";
+    String subwayNumInfo="";
+    JSONObject subwayInfo=null;
+    JSONArray jsonArray=null;
     String carNum="";
-    String elderlySeats="";
     static int elderlySeatsLength=12;
     ImageView[] imageViewArray=new ImageView[12];
+    String elderlySeats="";
+    String[] elderlySeatsArray=new String[10];
     SwipeRefreshLayout swipeRefreshLayout;
 
     //메소드
@@ -40,18 +58,24 @@ public class ElderlySeatsActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 Log.d("리프레쉬","실행");
+                elderlySeatsLoad();
                 elderlySeatsUpdate();
                 swipeRefreshLayout.setRefreshing(false);
                 Log.d("리프레쉬","완료");
             }
         });
 
+        //서버 전송을 위한 설정
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         //칸번호와 노약자석 정보 로드
         intent=getIntent();
+        subwayNum=intent.getStringExtra("subwayNum");
         carNum=intent.getStringExtra("carNum");
-        elderlySeats=intent.getStringExtra("elderlySeats");
+        elderlySeatsLoad();
+        Log.d("지하철번호",subwayNum);
         Log.d("칸번호",carNum);
-        Log.d("노약자석",elderlySeats);
 
         //이미지뷰 로드
         imageViewArray[0]=(ImageView)findViewById(R.id.pokemon1);
@@ -84,6 +108,7 @@ public class ElderlySeatsActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 Log.d("리프레쉬","실행");
+                elderlySeatsLoad();
                 elderlySeatsUpdate();
                 swipeRefreshLayout.setRefreshing(false);
                 Log.d("리프레쉬","완료");
@@ -92,10 +117,11 @@ public class ElderlySeatsActivity extends AppCompatActivity {
 
         //칸번호와 노약자석 정보 로드
         intent=getIntent();
+        subwayNum=intent.getStringExtra("subwayNum");
         carNum=intent.getStringExtra("carNum");
-        elderlySeats=intent.getStringExtra("elderlySeats");
+        elderlySeatsLoad();
+        Log.d("지하철번호",subwayNum);
         Log.d("칸번호",carNum);
-        Log.d("노약자석",elderlySeats);
 
         //이미지뷰 로드
         imageViewArray[0]=(ImageView)findViewById(R.id.pokemon1);
@@ -128,6 +154,72 @@ public class ElderlySeatsActivity extends AppCompatActivity {
                 //imageViewArray[i].setColorFilter(null);
                 imageViewArray[i].setVisibility(View.INVISIBLE);
             }
+        }
+    }
+
+    public void elderlySeatsLoad(){
+        Log.d("량 데이터 로드", "실행");
+
+        URL url = null;
+        HttpURLConnection con = null;
+        StringBuilder sb = new StringBuilder();
+
+        //JSON타입으로 값을 담음
+        try {
+            json = new JSONObject("{'subwayNum':"+subwayInfo+"}");
+        } catch (JSONException e) {
+            Log.d("json", "error");
+        }
+        //String형태로 변환
+        subwayNumInfo = json.toString();
+        Log.d("지하철번호데이터", subwayNumInfo);
+        //url설정
+        try {
+            Log.d("ip주소 ", "203.233.196.139");
+            url = new URL("http://203.233.196.139:8888/densha/apiservice/json?subwaynum="+subwayNum);
+        } catch (MalformedURLException e) {
+            Toast.makeText(this, "잘못된 주소입니다!", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            //서버와 연결
+            Log.d("서버연결", "실행");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setUseCaches(false);
+            con.setConnectTimeout(10000);
+            con.setReadTimeout(10000);
+            Log.d("서버연결", "완료");
+            if (con != null) {
+                //수신
+                Log.d("수신", "실행");
+                if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStreamReader in = new InputStreamReader(con.getInputStream());
+                    int ch;
+                    while ((ch = in.read()) != -1) {
+                        sb.append((char) ch);
+                    }
+                    in.close();
+                    Log.d("수신", "완료");
+
+                    //수신된 데이터 학인
+                    JSONObject jsonObject = new JSONObject(sb.toString());
+                    String result = jsonObject.getString("item");
+                    Log.d("수신데이터들",result);
+                    jsonArray=new JSONArray(result);
+                    subwayInfo=new JSONObject();
+
+                    //인원수와 혼잡도, 노약자석
+                    Log.d("수신데이터" + (carNum), jsonArray.get(Integer.parseInt(carNum)-1).toString());
+                    subwayInfo = (JSONObject) jsonArray.get(Integer.parseInt(carNum)-1);
+                    for (int j = 1; j <= 12; j++) {
+                        elderlySeats += subwayInfo.getString("elderlySeat" + j);
+                    }
+                    Log.d("노약자석",elderlySeats);
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.toString(), Toast.LENGTH_SHORT).show();
+            Log.d("오류",e.toString());
         }
     }
 }
